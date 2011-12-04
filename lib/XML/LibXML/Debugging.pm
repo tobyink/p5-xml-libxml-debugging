d476107ec30e49de4ead0fdad8bef9f66775eb45
Toby Inkster <mail@tobyinkster.co.uk>
2011-12-04 00:16:14 +0000

    Upgrade M:P:R
    --HG--
    branch : XML-LibXML-Debugging

--- lib/XML/LibXML/Debugging.pm
+++ lib/XML/LibXML/Debugging.pm
@@ -9,7 +9,7 @@ use XML::LibXML qw(:all);
 
 BEGIN {
 	$XML::LibXML::Debugging::AUTHORITY = 'cpan:TOBYINK';
-	$XML::LibXML::Debugging::VERSION   = '0.101';
+	$XML::LibXML::Debugging::VERSION   = '0.102';
 }
 
 sub XML::LibXML::Document::toDebuggingHash
